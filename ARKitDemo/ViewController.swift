fb07ae5c7883f4039f7452280e02f212a03f55d5
Vincent Villalta <vincentvillalta@me.com>
2018-01-08 09:08:33 -0600

    Initial Commit

---
+++ ARKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitDemo
+//
+//  Created by Vincent Villalta on 1/8/18.
+//  Copyright © 2018 Vincent Villalta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
